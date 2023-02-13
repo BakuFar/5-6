addd2e0e56a5e105b8e3566089d4f4fc7709dd79
Farhat <baku.far@gmail.com>
2023-02-13 19:03:34 +0600

    Initial Commit

---
+++ 5-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5-6
+//
+//  Created by Фархат Сталбек уулу on 13/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
